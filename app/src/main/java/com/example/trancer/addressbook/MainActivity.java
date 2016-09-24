489c1568443420a42153f0bc5567dd1472914f3b
“Ryan <ryancarlile@gmail.com>
2016-09-23 22:10:12 -0700

    Initialize repository and set up main activity and NewAddressActivity

---
+++ app/src/main/java/com/example/trancer/addressbook/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.trancer.addressbook;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.btnAddress) Button mAddAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mAddAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewAddressActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/trancer/addressbook/NewAddressActivity.java
@@ -0,0 +1,13 @@
+package com.example.trancer.addressbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewAddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_address);
+    }
+}
