29b801d39597e508672fb7037cdebb3836f0d36e
“Ryan <ryancarlile@gmail.com>
2016-09-27 13:45:25 -0700

    Add firebase recyclerview adapter and now address are being displayed

--- app/src/main/java/com/example/trancer/addressbook/MainActivity.java
+++ app/src/main/java/com/example/trancer/addressbook/MainActivity.java
@@ -3,20 +3,33 @@ package com.example.trancer.addressbook;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.btnAddress) Button mAddAddress;
+    private DatabaseReference mAddressReference;
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
+
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mAddressReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_ADDRESS);
+        setUpFirebaseAdapter();
 
         mAddAddress.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,7 +40,25 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public static class Constants {
-        public static final String FIREBASE_ADDRESS = "address";
+    private void setUpFirebaseAdapter() {
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Address, FirebaseAddressViewHolder>
+                (Address.class, R.layout.address_list, FirebaseAddressViewHolder.class,
+                        mAddressReference) {
+
+            @Override
+            protected void populateViewHolder(FirebaseAddressViewHolder viewHolder,
+                                              Address model, int position) {
+                viewHolder.bindAddress(model);
+            }
+        };
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
     }
 }
