a8783f9b08e28c88c71d71342e09f994c87e3d37
“Ryan <ryancarlile@gmail.com>
2016-09-28 17:48:53 -0700

    Add delete function to details page

--- app/src/main/java/com/example/trancer/addressbook/AddressDetailActivity.java
+++ app/src/main/java/com/example/trancer/addressbook/AddressDetailActivity.java
@@ -1,9 +1,12 @@
 package com.example.trancer.addressbook;
 
+import android.content.Intent;
 import android.os.Parcel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -12,6 +15,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import org.parceler.Parcels;
@@ -23,14 +27,16 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class AddressDetailActivity extends AppCompatActivity {
+
     private DatabaseReference mAddressReference;
-    private FirebaseRecyclerAdapter mFirebaseAdapter;
+    private ValueEventListener mAddressReferenceListener;
+
     @Bind(R.id.fullNameTV) TextView mFullName;
     @Bind(R.id.addressTV) TextView mAddressTV;
     @Bind(R.id.zipTV) TextView mStateAndZipTV;
-    private String mPostition;
-    ArrayList<Address> mAddress = new ArrayList<>();
+    @Bind(R.id.deleteBtn) Button mDelete;
 
+    private ArrayList<Address> mAddress = new ArrayList<>();
 
 
     @Override
@@ -40,12 +46,12 @@ public class AddressDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_address_detail);
         ButterKnife.bind(this);
 
-        int startingPosition = getIntent().getIntExtra("position", 0);
+        final int startingPosition = getIntent().getIntExtra("position", 0);
         mAddress = Parcels.unwrap(getIntent().getParcelableExtra("address"));
 
-        Address detail = mAddress.get(startingPosition);
+        final Address detail = mAddress.get(startingPosition);
 
-        Log.d("starting Position", startingPosition +"");
+        Log.d("starting Position", startingPosition + "");
         Log.d("parcel", detail + "");
 
         String firstName = detail.getFirstName();
@@ -63,50 +69,47 @@ public class AddressDetailActivity extends AppCompatActivity {
 
 
 
-        mAddressReference = FirebaseDatabase.getInstance().getReference("address");
-//        mAddressReferenceListener = mAddressReference.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                for (DataSnapshot addressSnapshot : dataSnapshot.getChildren()) {
-//                    Address address = addressSnapshot.getValue(Address.class);
-//                    String noteName = note.getName();
-//                    String body = note.getBody();
-//                    Map<String, Boolean> tags = note.getTags();
-//                    NoteModel newNote = new NoteModel(noteName, body, tags);
-//                    mLevelOneNotes.add(newNote);
-//                    Log.d("name------", newNote + "");
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        })
+        mDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                mAddressReference = FirebaseDatabase.getInstance().getReference("address");
 
-//        setUpFirebaseAdapter();
+//                mAddressReference.child(queryRef).removeValue();
+//                Log.d("queryRef", queryRef +"");
+                mAddressReferenceListener = mAddressReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+//                     Address node = dataSnapshot.child("address").getValue(Address.class);
+
+                        for (DataSnapshot addressSnapshot : dataSnapshot.getChildren()) {
+                            Address node = addressSnapshot.getValue(Address.class);
+
+                            if(node.getFirstName().equals(detail.getFirstName())&&node.getLastName().equals(detail.getLastName())&&node.getAddress().equals(detail.getAddress())&&node.getCity().equals(detail.getCity())&&node.getState().equals(detail.getState())&&node.getZip().equals(detail.getZip())) {
+                                String key = addressSnapshot.getKey();
+                                mAddressReference.getRef().child(key).removeValue();
+                                Log.d("data node", mAddressReference.child(key).toString());
+//                                node.child(key).removeValue();
+                            }
+//                            Map<String, Boolean> firstName = node.getFirstName();
+//                            for (String key: tags.keySet()) {
+//                                if(key.equals(mLevelOneTag.toLowerCase())){
+
+//                        final Query queryRef = dataSnapshot.orderByChild("address").equalTo(detail.getFirstName());
+//                        dataSnapshot.child(queryRef).remove();
+
+//                        node.getRef().setValue(null);
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                Intent intent = new Intent(AddressDetailActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-//
-//    private void setUpFirebaseAdapter() {
-//        mFirebaseAdapter = new FirebaseRecyclerAdapter<Address, FirebaseAddressViewHolder>
-//                (Address.class, R.layout.activity_address_detail, FirebaseAddressViewHolder.class,
-//                        mAddressReference) {
-//
-//            @Override
-//            protected void populateViewHolder(FirebaseAddressViewHolder viewHolder,
-//                                              Address model, int position) {
-//                viewHolder.bindAddress(model);
-//
-//            }
-//        };
-//
-//    }
-//
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        mFirebaseAdapter.cleanup();
-//
-//    }
 }
